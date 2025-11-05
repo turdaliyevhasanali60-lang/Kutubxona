be2dc5c7a161644c87a9d30e3b6c8d79f53106a9
Hasanali Turdaliyev <turdaliyevhasanali60@gmail.com>
2025-11-05 15:13:06 +0500

    Updated urls.py and cleaned up routes

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-
 from .models import *
 admin.site.register(Talaba)
-admin.site.register(Muallif)
 admin.site.register(Kitob)
-admin.site.register(Kutubxonachi)
-admin.site.register(Record)
+
+@admin.register(Kutubxonachi)
+class KutubxonachiAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ism')
+    list_filter = ('id',)
+    search_fields = ('ism',)
+
+@admin.register(Muallif)
+class MuallifAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ism')
+    list_filter = ('id',)
+    search_fields = ('ism',)
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'olingan_sana', 'qaytarish_sana', 'talaba', 'kutubxonachi')
+    list_filter = ('qaytarish_sana',)
